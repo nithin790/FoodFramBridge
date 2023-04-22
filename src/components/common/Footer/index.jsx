eed9298c578a398cf7de2b33c3de3365477138ad
nithin790 <113250052+nithin790@users.noreply.github.com>
2023-04-22 14:47:48 -0500

    UI/UX major changes

--- src/components/common/Footer/index.jsx
+++ src/components/common/Footer/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Logo from '../Logo';
 import './styles.css';
+import { FaFacebookSquare, FaTwitterSquare, FaInstagramSquare } from 'react-icons/fa';
 
 const Footer = () => {
+  
   return (
     <footer>
       <p>
-       @COPYRIGHTS2023 WEBLAUCH.INDIA
+      <a href="https://www.facebook.com"><FaFacebookSquare size={32} /></a>
+      <a href="https://www.twitter.com"><FaTwitterSquare size={32} /></a>
+      <a href="https://www.instagram.com"><FaInstagramSquare size={32} /></a>
       </p>
       <Logo />
     </footer>
